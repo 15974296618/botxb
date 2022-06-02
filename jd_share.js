c00be274fb27606fdbdd11e9b81c5802b3ee39e0
luomu <65743350+baquanluomu@users.noreply.github.com>
2022-06-02 22:08:19 +0800

    Update jd_share.js

--- jd_share.js
+++ jd_share.js
@@ -14,7 +14,8 @@ cron:1 1 1 1 *
 ============Quantumultx===============
 [task_local]
 #LZ分享有礼
-1 1 1 1 * jd_share.js, tag=LZ分享有礼, enabled=true
+cron:1 1 1 1 *
+jd_share.js, tag=LZ分享有礼, enabled=true
 
 */
 const $ = new Env("LZ分享有礼-落幕");
